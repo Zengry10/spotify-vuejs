fbdcd4b0d9ca42bd67d4e1fd6e7e852cfc17a72c
Zengry10 <R.lemoust@outlook.fr>
2022-12-09 00:10:46 +0100

    page detail album end

--- src/views/AlbumID.vue
+++ src/views/AlbumID.vue
@@ -16,13 +16,15 @@ async function init() {
 
 init()
  
+
+
 </script>
 <template>
     
   
        
-            <div key="id" class="bg-black h-60 flex">
-            <img :src="response.images[0].url" alt="" class="h-52 w-48 ml-12 mt-3">
+            <div v-if="response && response.images" key="id" class="bg-gradient-to-t from-slate-800 to-slate-900 h-60 flex ">
+                <img :src="response.images[0].url" alt="" class="h-52 w-48 ml-12 mt-3">
                 <div class="flex-col ml-4">
                     <p class="text-white mt-16 mb-2 uppercase font-black">{{ response.album_type }}</p>
                     <p class="text-white text-6xl">{{ response.name }}</p>
@@ -33,24 +35,34 @@ init()
                     </div>
                 </div>
             </div>
-            <div class="bg-slate-800 h-36 pl-12">
-                <div class='flex justify-between'>
-                    <p class="uppercase text-slate-500"># &nbsp; &nbsp; title</p>
-                    <p class="ml-96 uppercase text-slate-500">plays</p>
-                    <p class="mr-8 text-slate-500">ouf</p>
-                </div>
-                <div id="ligne" class=""></div>
-                
-                <div v-for= "album in response">
-                    <pre>
-                        <p>{{ album}}</p>
-                    </pre>
-                </div>
+        <div class="bg-black  pl-12 ">
+            <div class='flex justify-between '>
+                <p class="uppercase text-slate-500 mt-12"># &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; title</p>
+                <p class="ml-96 uppercase text-slate-500 mt-12">plays</p>
+                <p class="mr-28 text-slate-500 mt-12">times</p>
             </div>
+                     <div id="ligne" class="mb-4"></div>
+
+            
+            <div v-if="response && response.tracks" >
+                <div v-for="album in response.tracks.items" class="flex justify-between">
+                    <div class="flex">
+                        <p class=" text-slate-600 mt-3 mr-12"> {{ album.track_number }}</p>
+                        <div class="">
+                            <p class="text-lg text-white"> {{ album.name }}</p>   
+                            <div v-for="artist in album.artists">
+                                <h4 class="text-white text-slate-600">{{ artist.name }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text-white mr-28" >{{ Math.floor((album.duration_ms/1000/60) << 0)}}:<span v-if="Math.floor((album.duration_ms/1000) % 60) < 10">0</span>{{ Math.floor((album.duration_ms/1000) % 60)}}</p>
+                    </div>
+                </div>
+            </div>  
+        </div>
            
-            <!-- <div v-if="response" v-for="album in response.tracks.items" key="aze">
-                {{ album.duration_ms }}
-            </div> -->
+
         
 
             
